1726419686505fabbccad56fc7cc3d5c8d4a04d5
sap-prajapati <saloniprajapati98@gmail.com>
2022-05-30 20:43:37 +0530

    Assignment3.3 - count to be printed as 3 inside log method.

--- Week-3/Assignment3.3.js
+++ Week-3/Assignment3.3.js
@@ -1,10 +1,12 @@
 function createIncrement() {
     let count=0;
+    var message;
     function increment(){
         count++;
         console.log(count);
+         message=`Count is ${count}`;
     }
-    let message=`Count is ${count}`;
+    
     function log(){
         console.log(message);
     }
@@ -15,7 +17,7 @@ increment(); //incremented count to 1
 increment(); //incremented count to 2
 increment(); //incremented count to 3
 log();// What is logged?  - Count is 0 because this message string is created only once when createIncrement funtion is called.
-
+//now message variable is in module scope so it will change everytime whenever increment funtion is called , that is why output is count is 3.
 
 
 //Output - Count is 0
