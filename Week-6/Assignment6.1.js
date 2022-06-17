85cd725a2446510acd8db246ed3290fb1f39f0b6
sap-prajapati <saloniprajapati98@gmail.com>
2022-06-17 15:54:15 +0530

    Assignment6.1 - Max Sum Contiguous Subarray

---
+++ Week-6/Assignment6.1.js
@@ -0,0 +1,21 @@
+function maxSubArraySum(a, size)
+{
+    var maxint = Math.pow(2, 53)
+    var max_so_far = -maxint - 1
+    var max_ending_here = 0
+      
+    for (var i = 0; i < size; i++)
+    {
+        max_ending_here = max_ending_here + a[i]
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here
+ 
+        if (max_ending_here < 0)
+            max_ending_here = 0
+    }
+    return max_so_far
+}
+  
+
+ var a = [ -2, 1, -3, 4, -1, 2, 1, -5, 4 ];
+console.log("Maximum contiguous sum is", maxSubArraySum(a, a.length));
