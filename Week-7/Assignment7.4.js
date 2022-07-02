ceae5ed3f15a9401237aba9183fa3b3cd20ba88c
Saloni Prajapati <saloniprajapati98@gmail.com>
2022-07-02 15:12:09 +0530

    Assignment7.4-formatted

--- Week-7/Assignment7.4.js
+++ Week-7/Assignment7.4.js
@@ -1,49 +1,46 @@
 
-function areBracketsBalanced(expr)
-{
-	
+function areBracketsBalanced(expr) {
+
 
 	let stack = [];
 
-	
-	for(let i = 0; i < expr.length; i++)
-	{
+
+	for (let i = 0; i < expr.length; i++) {
 		let x = expr[i];
 
-		if (x == '(' || x == '[' || x == '{')
-		{
-			
-		
+		if (x == '(' || x == '[' || x == '{') {
+
+
 			stack.push(x);
 			continue;
 		}
 
-	
+
 		if (stack.length == 0)
 			return false;
-			
+
 		let check;
-		switch (x){
-		case ')':
-			check = stack.pop();
-			if (check == '{' || check == '[')
-				return false;
-			break;
+		switch (x) {
+			case ')':
+				check = stack.pop();
+				if (check == '{' || check == '[')
+					return false;
+				break;
 
-		case '}':
-			check = stack.pop();
-			if (check == '(' || check == '[')
-				return false;
-			break;
+			case '}':
+				check = stack.pop();
+				if (check == '(' || check == '[')
+					return false;
+				break;
 
-		case ']':
-			check = stack.pop();
-			if (check == '(' || check == '{')
-				return false;
-			break;
-		default:
+			case ']':
+				check = stack.pop();
+				if (check == '(' || check == '{')
+					return false;
+				break;
+			default:
 				console.log(`wrong expression`);
-		
+
 		}
 	}
 
